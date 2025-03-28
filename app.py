31553086a3107a7b232025aff3be3bb76eaf71b7
Edna Sanchez <tu.loredna@gmail.com>
2025-03-27 19:05:58 -0700

    Mi primer commit

---
+++ app.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+df = pd.read_csv(r'../datasets/vehicles_us.csv')
+
+if hist_button: # al hacer clic en el botón
+            # escribir un mensaje
+            st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+            
+            # crear un histograma
+            fig = px.histogram(car_data, x="odometer")
+        
+            # mostrar un gráfico Plotly interactivo
+            st.plotly_chart(fig, use_container_width=True)
