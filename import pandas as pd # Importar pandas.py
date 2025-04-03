548e9e87c65134e22e8d4d39bcc251b7e857dc5e
Edna Sanchez <tu.loredna@gmail.com>
2025-04-03 12:28:15 -0700

    cambios commit

--- import pandas as pd # Importar pandas.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd  # Importar pandas
-import plotly.express as px  # Importar plotly.express
-
-# Cargar el archivo CSV
-df = pd.read_csv(r'C:\Users\User\Documents\analisis de datos\Repositorio\analytics_portfolio-Edna-Sanchez-\render_app\vehicles_us.csv')
-
-# Crear un histograma
-fig_hist = px.histogram(df, x='price', title='Histograma de precio')
-fig_hist.show()
